6c768fb1dad41ef88b0ab81e5f1a6c36f2f934d3
John Cable <johnbcable@me.com>
2013-03-20 18:28:15 +0000

    Added touch features to app and revised styling to cope

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     showAlert: function(message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -21,23 +8,15 @@ var app = {
         }
     },
 
-    renderHomeView: function() {
-        var html = "<div class='header'><h1>Home</h1></div>" + 
-                   "<div class='search-view'>" + 
-                   "<input class='search-key'>" + 
-                   "<ul class='employee-list'></ul>" +
-                   "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore( function(){
             // self.showAlert('Store initialised', 'Info');
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+//        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+//        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
