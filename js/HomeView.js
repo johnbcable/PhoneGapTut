5c58d34078f35f9513e01ec48cd6c3639fc646fb
John Cable <johnbcable@me.com>
2013-03-21 10:55:43 +0000

    Replaced by step code from Chris Coenrates site

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,15 +1,15 @@
 var HomeView = function(store) {
 
-	this.initialize = function() {
-		// define a div wrapper for the view
-		this.el = $('<div />');
-		this.el.on('keyup', '.search-key', this.findByName);
-	};
+    this.initialize = function() {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
 
     this.render = function() {
         this.el.html(HomeView.template());
         return this;
-     };
+    };
 
     this.findByName = function() {
         store.findByName($('.search-key').val(), function(employees) {
@@ -17,7 +17,8 @@ var HomeView = function(store) {
         });
     };
 
-	this.initialize();
+    this.initialize();
+
 }
 
 HomeView.template = Handlebars.compile($("#home-tpl").html());
--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 var app = {
 
-    showAlert: function(message, title) {
+    showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
-            alert(title ? (title +": " + message) : message);
+            alert(title ? (title + ": " + message) : message);
         }
+    },
+
+    registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
     },
 
     route: function() {
@@ -22,43 +32,13 @@ var app = {
         }
     },
 
-    registerEvents: function() {
-        var self = this;
-        // Check if the browser supports touch events
-        if (document.documentElement.hasOwnProperty('ontouchstart')) {
-            // yes, so register the touch event listener
-            $('body').on('touchstart', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('touchend', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-        } else {
-            // no, so register the mouse events instead
-            $('body').on('mousedown', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('mouseup', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-        }
-        $(window).on('hashchange', $.proxy(this.route, this));
-    },
-
     initialize: function() {
         var self = this;
-        var detailsURL = /^#employees\/(\d{1,})/;
+        this.detailsURL = /^#employees\/(\d{1,})/;
         this.registerEvents();
-        this.store = new LocalStorageStore( function() {
-            // self.showAlert('Store initialised', 'Info');
-            // $('body').html(new HomeView(self.store).render().el);
+        this.store = new MemoryStore(function() {
             self.route();
         });
-//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-//        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-//        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
-
     }
 
 };
