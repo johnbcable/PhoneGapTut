df85792251d6edc7d6edc3841e27958bef36082b
John Cable <johnbcable@me.com>
2013-03-25 13:15:12 +0000

    Updated with Part 13 of the tutorial files

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -42,31 +42,30 @@ var EmployeeView = function(employee) {
         return false;
     };
 
-    this.changePicture = function (event) {
-    	event.preventDefault();
-    	if (!navigator.camera) {
-    		app.showAlert("Camera API not supported", "Error");
-    		return;
-    	}  
+    this.changePicture = function(event) {
+        event.preventDefault();
+        console.log('changePicture');
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+        var options =   {   quality: 50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                            encodingType: 0     // 0=JPG 1=PNG
+                        };
 
-    	var options = {
-    		quality: 50,
-    		destinationType: Camera.DestinationType.DATA_URL,
-    		sourceType: 1,   //  0: Photo library, 1= camera
-    		encodingType: 0  // 0=jpg, 1=png
-    	};
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('#image').attr('src', "data:image/jpeg;base64," + imageData);
+            },
+            function() {
+                alert('Error taking picture');
+            },
+            options);
 
-    	navigator.camera.getPicture( 
-    		function(imageData) {
-          	 	$('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
-        },    		},
-    		function() {
-    			app.showAlert('Error taking picture', 'Error');
-    		},
-    		options);
-
-    	return false;
-    }
+        return false;
+    };
 
     this.initialize();
 
